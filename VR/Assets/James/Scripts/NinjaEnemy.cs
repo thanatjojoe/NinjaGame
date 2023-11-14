c2238a35d5e1c4377ba3b1f8d06abe083f69e2f3
MrJOKIT <mrjokit@gmail.com>
2023-11-15 01:08:27 +0700

    EnemyPathfinding

--- VR/Assets/James/Scripts/NinjaEnemy.cs
+++ VR/Assets/James/Scripts/NinjaEnemy.cs
@@ -2,16 +2,53 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NinjaEnemy : MonoBehaviour
 {
+    [SerializeField] private Transform detectTrans;
+    [SerializeField] private float detectRadius;
+    [SerializeField] private LayerMask playerLayer;
+
+    private bool onAttack;
+    
     private Rigidbody _rigidbody;
     private Animator _animator;
-    
-    private void Start()
+    private NavMeshAgent _agent;
+    private Transform playerTransform;
+
+    private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
+        _agent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        _agent.SetDestination(playerTransform.position);
+        AttackMode();
+    }
+
+    private void AttackMode()
+    {
+        Collider[] playerDetect = Physics.OverlapSphere(detectTrans.position, detectRadius, playerLayer);
+        foreach (Collider player in playerDetect)
+        {
+            if (player.gameObject.CompareTag("Player"))
+            {
+                if (!onAttack)
+                {
+                    StartCoroutine(Attack());
+                }
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -31,7 +68,20 @@ public class NinjaEnemy : MonoBehaviour
 
     IEnumerator DestoryTime()
     {
-        yield return new WaitForSeconds(10f);
+        yield return new WaitForSeconds(5f);
         Destroy(gameObject);
+    }
+
+    IEnumerator Attack()
+    {
+        onAttack = true;
+        Debug.Log("Attack!!!");
+        yield return new WaitForSeconds(5f);
+        onAttack = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(detectTrans.position,detectRadius);
     }
 }
